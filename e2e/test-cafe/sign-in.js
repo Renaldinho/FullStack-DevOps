02891b484d94ebb2d51ef38e4a32cd12033374ee
Renaldinho <rena0968@easv365.dk>
2023-05-03 18:39:25 +0200

    Revert "yayyy"
    This reverts commit e558ceeab7761b5f86a48a193efc151879d2ec2c.

--- e2e/test-cafe/sign-in.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'testcafe';
-
-fixture `My First TestCafe Test`
-
-test.page('http://localhost:4200/')
-
-test('Check if Angular app is loaded', async t => {
-  const appRoot = Selector('app-root');
-});
