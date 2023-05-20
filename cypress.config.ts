36d45b772761bf69cda86fbdac7238fa2908189e
Renaldinho <rena0968@easv365.dk>
2023-05-20 06:57:57 +0200

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,15 +4,5 @@ export default defineConfig({
 
   e2e: {
     baseUrl: "http://app:8000"
-  },
-
-
-  component: {
-    devServer: {
-      framework: 'angular',
-      bundler: 'webpack',
-    },
-    specPattern: '**/*.cy.ts'
   }
-
 })
