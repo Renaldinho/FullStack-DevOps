022b2c089edc384d824da0f63c822d372c9e0494
Renaldinho <rena0968@easv365.dk>
2023-05-20 07:40:40 +0200

    .

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-
-  e2e: {
-    'baseUrl': 'http://localhost:8000'
-  },
-
-
+  baseUrl: 'http://app:8000',
   component: {
+    componentFolder: 'src',
+    testFiles: '**/*.cy.ts',
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
-    specPattern: '**/*.cy.ts'
-  }
-
+  },
 })
